68e9bc02e3c0696bfed8664df722176807fa9427
crochetcode <fred.davison@outlook.com>
2022-06-13 15:39:53 -0500

    :bug: fix stream panel

--- src/pages/stream.js
+++ src/pages/stream.js
@@ -1,9 +1,17 @@
 import {Panel} from '../components/panel';
+import styled from 'styled-components';
+
+const FlexDiv = styled.div`
+    display: flex;
+    margin-bottom: 150px;
+`;
 
 export const Stream = props => {
     const {playerInfo, matchHistory, loading} = props;
 
     return (
-        <Panel playerInfo={playerInfo} matchHistory={matchHistory} loading={loading}/>
+        <FlexDiv>
+            <Panel playerInfo={playerInfo} matchHistory={matchHistory} loading={loading}/>
+        </FlexDiv>
     );
 };

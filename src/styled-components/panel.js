8d68c31fe32c8a5e190bc95628ddad08da377759
crochetcode <fred.davison@outlook.com>
2022-06-15 21:31:09 -0500

    :lipstick: inset

--- src/styled-components/panel.js
+++ src/styled-components/panel.js
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
 
 export const StyledPanel = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
     margin-right: 15px;
-    background: lightgrey;
+    background: lightgray;
     box-sizing: border-box;
     box-shadow: 5px 5px black;
-    height: 325px;
+    border: 3px outset white;
     h1 {
-        font-size: 4rem;
+        font-size: 2rem;
     }
     h2 {
         font-size: 25px;
@@ -16,8 +19,11 @@ export const StyledPanel = styled.div`
         background: black;
         color: white;
     }
-
     .content {
-        padding: 20px 35px;
+        padding: 15px;
+        background: white;
+        margin: 5px;
+        border: 3px inset white;
+        height: 75%;
     }
 `;

5a5a916e33309935a3788afa13e53740e551e556
crochetcode <fred.davison@outlook.com>
2022-06-15 22:17:56 -0500

    :lipstick: font

--- src/styled-components/panel.js
+++ src/styled-components/panel.js
@@ -1,23 +1,28 @@
 import styled from 'styled-components';
 
 export const StyledPanel = styled.div`
+    font-family: 'IBM Plex Mono', monospace;
     display: flex;
     flex-direction: column;
     justify-content: start;
-    margin-right: 15px;
+    margin-right: 20px;
     background: lightgray;
     box-sizing: border-box;
     box-shadow: 5px 5px black;
     border: 3px outset white;
     h1 {
-        font-size: 2rem;
-    }
+        font-size: 3rem;
+        font-weight: 800;
+        // font-family: 'Press Start 2P', cursive;
+    }   
     h2 {
         font-size: 25px;
     }
     marquee {
         background: black;
         color: white;
+        font-family: 'Papyrus';
+        font-size: 1.05rem;
     }
     .content {
         padding: 15px;
